13c949d674b48504aba2b0916627e24cb2c35f15
smithg26 <smithg26@uww.edu>
2014-09-24 13:47:40 -0500

    Version 1.1

--- GuessingGameLab1/GuessingGame.java
+++ GuessingGameLab1/GuessingGame.java
@@ -2,19 +2,29 @@ import java.util.*;
 public class GuessingGame {
 	public static void main(String [] args){
 		Scanner input=new Scanner(System.in);
+		int guess=0;		
 		int x=(int)(Math.random()*10);
-		System.out.print("Please guess an integer between 0 and 10");
-		int guess=input.nextInt();
+		while(guess!=x||guess!=-1){
+		System.out.print("Please guess an integer between 0 and 10 or enter -1 to exit.");		
+		int guess=input.nextInt();			
 		if(guess<0||guess>10){
-			System.out.print("Entry must be between 0 and 10.  You failed.");
+			System.out.print("Guess must be between 0 and 10.");
 		}
 		else if(guess==x){
 			System.out.println("Congratulations! You guessed right!");
 		}
-		else{
-			System.out.println("Sorry, the correct answer was "+x+".  Goodbye.");
+		else if(guess>x){
+			System.out.print("Sorry, the correct answer was lower than your guess.  Guess again or enter -1 to exit..");
 		}
+		else if(guess<x){
+			System.out.print("Sorry, the correct answer was lower than your guess.  Guess again or enter -1 to exit..");
 			
-	}
+		}
+		}
+		
+			System.out.println("Thank you for playing. Goodbye.");
+			System.exit(1);
+		}
+}
 
 }
